dcd9388f1fb5ac3bc8a68b3159f17293021dc653
Chester2171 <68295603+Chester2171@users.noreply.github.com>
2020-08-04 20:11:21 -0700

    Add files via upload
    Create a method that returns the count of unique & distinct elements in an int array.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,57 @@
 ﻿using System;
 
-namespace AlgoExpert
+namespace CountDestinctElements
 {
-    class Program
+    class ArrayNumberAlgorithms
     {
-
-        public static int FindTheNumber(int[] array)
+        //Create a method that returns the count of unique & distinct elements in an int array.
+    
+        public static int UniqueNumber(int[] array)
         {
-            for (int i =0; i < array.Length; i++)
+            int count = 0;
+    
+            for (int i = 0; i < array.Length; i++)
             {
-                int count = 0;
+                int duplicates = 0;
+    
                 for (int j = 0; j < array.Length; j++)
                 {
                     if (i != j)
                     {
-                        if (array[i] == array[j])
-                        {
-                            count++;
-                        }
+                        if (array[i] == array[j]) duplicates++;
                     }
                 }
-                if (count == 0)
+    
+                if (duplicates == 0) count++;
+            }
+    
+            return count;
+        }
+        public static int DistinctNumbers(int[] DistinctArray)
+        {
+    
+            int counter = 0;
+            int length = DistinctArray.Length;
+    
+            for (int i = 0; i < DistinctArray.Length; i++)
+            {
+                for (int j = i + 1; j < DistinctArray.Length; j++)
                 {
-                    return array[i];
+                    if (DistinctArray[i] == DistinctArray[j])
+                    {
+                        counter++;
+                    }
                 }
             }
-            return 0;
+            return length - counter;
         }
-
+    
         static void Main(string[] args)
         {
-            int[] testArray = new int[] { 1, 1, 2, 2, 4, 6, 6, 8, 8, 10, 10 };
-            Console.WriteLine($"{FindTheNumber(testArray)}");
+            int[] testArray = new int[] { 8, 6, 7, 5, 3, 0, 9, 8, 6, 7 };
+    
+            Console.WriteLine($"There are {UniqueNumber(testArray)} unique elements");
+            Console.WriteLine($"There are {DistinctNumbers(testArray)} distinct elements");
         }
     }
-} 
+}
